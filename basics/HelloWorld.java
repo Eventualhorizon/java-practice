0c2802f7015bcb36155a471f56df30feddcd9808
Asaph Bashioum <asaph.bashioum@gmail.com>
2026-07-17 14:14:34 -0400

    added Coordinates object to hold x and y values of array elements, added method to determine distance between two points on a two-dimensional plane, updated second array solver to track total distance travelled rather than comparing coordinate with previous output, added loop in second array solver to independently measure the distance travelled from 0th to 4th element in array before the main loop comparing the 5th to final elements in array

--- basics/HelloWorld.java
+++ basics/HelloWorld.java
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 package basics;
 
 public class HelloWorld {
     public static void main (String[] args){
         System.out.println("Hello, Java!");
     }
-=======
-package basics;
-
-public class HelloWorld {
-    public static void main (String[] args){
-        System.out.println("Hello, Java!");
-    }
->>>>>>> 3af9fbad8e2b4e8577a89654c62403d719686c58
 }
--- basics/KelvinArray.java
+++ basics/KelvinArray.java
@@ -17,25 +17,64 @@ package basics;
 
 public class KelvinArray {
 
+    //create an object to represent coordinates on a two-dimensional plane 
+    public class Coordinate {
+        double x;
+        double y;
+
+        public Coordinate(double x, double y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+    //measure distance between two coordinates on two-dimensional plane by adding the squares of the differences in x values and y values
+    public static double measureDistance(Coordinate coordinateOne, Coordinate coordinateTwo){
+        double measuredDistance = Math.sqrt(Math.pow((coordinateOne.x - coordinateTwo.x), 2) + (Math.pow((coordinateOne.y - coordinateTwo.y), 2)));
+        return measuredDistance;
+    }
+
+    //output whenever certain distance from previous output has been reached, first output is 0th element of array
     public static void processArrayOne ([] arrayOne){
-        final int distance; //this is the specified distance to be reached between coordinates
-        int output = arrayOne[0]; //starting output
+        final double distance; //this is the specified distance to be reached between coordinates
+        Coordinate output = new Coordinate(arrayOne[0].x, arrayOne[0].y); //first output is the 0th element of the array
+        System.out.println(output.c, output.y); //initial output is the 0th element of the array
+
         for (int i=1; i < arrayOne.length; i++){
-            if(math.abs((arrayOne[i] - output)) >= distance){
+
+            //for each iteration, measure distance by adding the squares of the differences in x values and y values
+            double measuredDistance = measureDistance(arrayOne[i], output);
+
+            //if the distance between the ith element of the array and the current 'output' variable is greater
+            //than the specified distance, update the current output to be the ith element and print the coordinates
+            if(measuredDistance >= distance){
                 output = arrayOne[i];
-                System.out.println(output);
+                System.out.println("Distance reached at array index " + i);
             }
         }
     }
 
+    //output whenever certain distance has been travelled across past 5 indices
     public static void processArrayTwo([] arrayTwo){
-        final int distance; //this is the specified distance to be reached between coordinates
-        int startingPoint = arrayOne[0]; //starting output
-        for (int i=1; i < arrayTwo.length; i++){
-            if((i >= 5) && math.abs((arrayTwo[i] - startingPoint)) >= distance){
-                System.out.println(output);
+        final double distance; //this is the specified distance to be travelled after five indices
+        Coordinate startingPoint = arrayOne[0]; //start at first element of array
+        double distanceTravelled; //initialize variable to keep track of distance travelled past coordinates
+
+        //we will start by measuring the distance travelled after the 0th through 4th indices
+        for (int i = 1; i<5; i++){
+            distanceTravelled += measureDistance(arrayTwo[i], arrayTwo[i-1]);
+        }
+        if (distanceTravelled >= distance){
+            System.out.println("Distance travelled after array index 4");
+        }
+        //for array indices 5 onward, continue to update distanceTravelled variable by adding distance travelled from
+        //i-1 to ith element and subtracting the distance travelled from i-5th to i-4th element
+        for (int i=5; i < arrayTwo.length; i++){
+            distanceTravelled += measureDistance(arrayTwo[i], arrayTwo[i-1])//add distance from i-1th to ith element
+            distanceTravelled -= measureDistance(arrayTwo[i-4], arrayTwo[i-5]); //subtract distance from i-5th to i-4th element
+            if(distanceTravelled >= distance){
+                System.out.println("Distance travelled after array index " + i);
             }
-            startingPoint = arrayTwo[i-5];
+            }
         }
     }
-}
+
