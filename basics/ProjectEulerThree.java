8395ca558eba17fb8361efd930fdb839ca744c79
Asaph Bashioum <asaph.bashioum@gmail.com>
2026-08-04 15:12:06 -0400

    wrote class to test a number for primality using sieve of eratosthenes

--- basics/ProjectEulerThree.java
+++ basics/ProjectEulerThree.java
@@ -10,13 +10,38 @@ package basics;
 
 
 public class ProjectEulerThree {
+
+    //class to determine if a number is prime or not using sieve of eratosthenes
+    //take a number, determine its square root (rounded down)
+    //then check every number from the square root to 2 to see if it is a factor
+    //returns true if it has no factors i.e. is a prime number, false otherwise
+    public static boolean primeChecker(double number){
+        double squareRoot = Math.floor(Math.sqrt(number)); //round down square root
+        while(squareRoot < 1){
+            if(number % squareRoot ==0){
+                //System.out.println(number + "is not prime, has divisor " + squareRoot);
+                return false;
+            }
+            squareRoot--;
+        }
+        //System.out.println(number + "is prime");
+        return true;
+    }
     public static void main(String [] args){
         final long number = 600851475143L; //have to add L to a literal in java or else it's automatically cast as int
-        final double testMultiple = Math.floor(Math.sqrt(number));
-        while(testMultiple > 0){
-            if(number%testMultiple == 0){
-                
+        double numberBeingTested = Math.floor(Math.sqrt(number));
+        double currentLargestPrime = 0; //keep track of the largest prime number
+        while(numberBeingTested > 1){
+            if(number%numberBeingTested == 0){
+                double otherDivisor = number/numberBeingTested; //create a variable for the other divisor
+                if(primeChecker(numberBeingTested) && numberBeingTested < otherDivisor){
+                    currentLargestPrime = numberBeingTested;
+                } else if(primeChecker(otherDivisor)){
+                    currentLargestPrime = otherDivisor;
+                }
             }
+            numberBeingTested-=1;
         }
+        System.out.println(currentLargestPrime);
     }
 }
